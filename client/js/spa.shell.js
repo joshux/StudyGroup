77c22c8c69801a64e25d3abb62a13b4c8be2056c
Yu-Shih Wang <joshux321@gmail.com>
2015-05-19 19:02:12 +0800

    3.6 final

--- client/js/spa.shell.js
+++ client/js/spa.shell.js
@@ -1,3 +1,7 @@
+var $ = require('jquery');
+var uriAnchor = require('urianchor');
+var _ = require('lodash');
+
 var configMap = {
   main_html: String()
     + '<div class="spa-shell-head">'
@@ -17,11 +21,15 @@ var configMap = {
     chat_extend_height: 450,
     chat_retract_height: 15,
     chat_extended_title: 'Click to retract',
-    chat_retracted_title: 'Click to extend'
+    chat_retracted_title: 'Click to extend',
+    anchor_schema: { 
+      chat: { open: true, closed: true }
+    }
 };
 var stateMap = { 
   $container: null,
-  is_chat_retracted: true
+  is_chat_retracted: true,
+  anchorMap: {}
 };
 var jqueryMap = {};// no need {}?
 var setJqueryMap = function(){
@@ -68,10 +76,73 @@ var toggleChat = function(do_extend, callback){
 };
 
 var onClickChat = function(event){
-  toggleChat(stateMap.is_chat_retracted);
+  changeAnchor({chat: (stateMap.is_chat_retracted ? 'open':'closed') });
   return false;
 };
 
+var changeAnchor = function(anchorMap){
+  var newAnchorMap = _.cloneDeep(stateMap.anchorMap);
+  _.forEach(anchorMap, function(v, k){
+    if(v.indexOf('_') === 0)
+      return;
+    newAnchorMap[k] = v;
+    var k_dep = '_' + k;
+    var s_k_dep = '_s' + k_dep;
+    delete newAnchorMap[s_k_dep];// no use for setAnchor
+
+    if(anchorMap[k_dep]){
+      newAnchorMap[k_dep] = _.clone(anchorMap[k_dep]); // shallow ok
+    } else{
+      delete newAnchorMap[k_dep];
+    }
+  });
+  try{
+    uriAnchor.setAnchor(newAnchorMap);
+  } catch(e){
+    uriAnchor.setAnchor(stateMap.anchorMap, null, true);
+    return false;
+  }
+  return true;
+};
+
+var onHashChange = function(){
+  var newAnchorMap;
+  try{
+    newAnchorMap = uriAnchor.makeAnchorMap();
+  } catch(e){
+    uriAnchorMap.setAnchor(stateMap.anchorMap, null, true);
+    return false;
+  }
+  var oldAnchorMap = _.cloneDeep(stateMap.anchorMap);
+  stateMap.anchorMap = newAnchorMap;
+  
+  //utilize chat state change
+  var diff = diffAnchorMap(newAnchorMap, oldAnchorMap, 'chat');
+  if(diff){
+    if(diff === 'open'){
+      toggleChat(true);
+    }
+    else if(diff === 'closed'){ // 'closed1' error
+      toggleChat(false);
+    }
+    else{
+      stateMap.anchorMap = oldAnchorMap;
+      uriAnchor.setAnchor(stateMap.anchorMap, null, true); // book error?
+      return false;
+    }
+  }
+  return true;
+};
+
+var diffAnchorMap = function(newAnchorMap, oldAnchorMap, key){
+  var key_dep = '_' + key;
+  var s_key_dep = '_s' + key_dep;
+
+  if(newAnchorMap[s_key_dep] && newAnchorMap[s_key_dep] !== oldAnchorMap[s_key_dep])
+    return newAnchorMap[s_key_dep];
+  else
+    return false;
+};
 
 module.exports = {
   initModule: function($container){
@@ -83,5 +154,11 @@ module.exports = {
     jqueryMap.$chat
       .attr('title', configMap.chat_retracted_title)
       .click(onClickChat);
+
+    uriAnchor.configModule({
+      schema_map: configMap.anchor_schema
+    });
+
+    $(window).bind('hashchange', onHashChange).trigger('hashchange');
   }
 };
